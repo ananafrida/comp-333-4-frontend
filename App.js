8a062e2efae583237c5e50a848cd62b73c5b45c9
ananafrida <anan.afrida.yes@gmail.com>
2023-11-11 03:00:44 -0500

    implement main page (issue #12)

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
+
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 
+
 import LoginPage from "./views/login";
 import RegisterPage from "./views/register";
-import MainPage from "./views/mainpage";
+import MainPage from "./views/main";
+import UpdatePage from "./views/update";
+import ReadPage from "./views/read";
+import DeletePage from "./views/delete";
+import CreatePage from "./views/create";
+
 
 export default function App() {
   const Stack = createStackNavigator();
+
 
   return (
     <NavigationContainer>
@@ -17,10 +25,15 @@ export default function App() {
         <Stack.Screen name="Login" component={LoginPage} />
         <Stack.Screen name="Register" component={RegisterPage} />
         <Stack.Screen name="Main" component={MainPage} />
+        <Stack.Screen name="Update" component={UpdatePage} />
+        <Stack.Screen name="Read" component={ReadPage} />
+        <Stack.Screen name="Delete" component={DeletePage} />
+        <Stack.Screen name="Create" component={CreatePage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
