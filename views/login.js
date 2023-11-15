59b3ef9470b95f64b658197f0251fc0bf59d8837
ananafrida <anan.afrida.yes@gmail.com>
2023-11-15 04:30:31 -0500

    commented codes (issue #10)

--- views/login.js
+++ views/login.js
@@ -1,3 +1,4 @@
+// Import necessary dependencies
 import React from "react";
 import { useForm } from "react-hook-form";
 import { useState, useEffect } from "react";
@@ -17,19 +18,24 @@ import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 
+// Create the functional component for LoginPage
 export default function LoginPage({ navigation }) {
+  // State variables for storing input values
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
 
+  // Function to handle navigation to the registration page
   const onRegisterClick = () => {
     navigation.navigate("Register");
   };
 
+  // Function to handle user login
   const onLogin = () => {
+    // Make a POST request to the server to perform login
     axios
       .post(
-        "http://172.21.229.212/index.php/user/login",
+        "http://172.21.9.38/index.php/user/login",
         {
           username: email,
           password: password,
@@ -38,17 +44,18 @@ export default function LoginPage({ navigation }) {
       )
       .then((response) => {
         if (response.data.success) {
-          // Successful login, set the user and store login data
-          console.log(response.data);
+          // Successful login, set the user and navigate to the "Main" screen
           setUsername(response.data.username);
           navigation.replace("Main", { username: response.data.username });
         } else {
+          // If login fails, show an error message
           Alert.alert("Login Failed", "Incorrect Username or Password", [
             { text: "OK", onPress: () => console.log("OK Pressed") },
           ]);
         }
       })
       .catch((error) => {
+        // If an error occurs during the request, show an error message
         Alert.alert("Login Failed", error, [
           { text: "OK", onPress: () => console.log("OK Pressed") },
         ]);
@@ -56,16 +63,19 @@ export default function LoginPage({ navigation }) {
       });
   };
 
+  // Render the component
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Login Page</Text>
       <KeyboardAwareScrollView>
+        {/* Input for entering the username */}
         <TextInput
           placeholder="Username"
           onChangeText={setEmail}
           value={email}
           style={styles.input}
         />
+        {/* Input for entering the password */}
         <TextInput
           placeholder="Password"
           onChangeText={setPassword}
@@ -73,6 +83,7 @@ export default function LoginPage({ navigation }) {
           secureTextEntry={true}
           style={styles.input}
         />
+        {/* Buttons for submitting the form and navigating to the registration page */}
         <View style={styles.button}></View>
         <Button title="Submit" onPress={onLogin} style={styles.button} />
         <View style={styles.button}></View>
@@ -86,6 +97,7 @@ export default function LoginPage({ navigation }) {
   );
 }
 
+// Styles for the component
 const styles = StyleSheet.create({
   container: {
     flex: 1,
--- views/register.js
+++ views/register.js
@@ -1,3 +1,4 @@
+// Import necessary dependencies
 import React from "react";
 import { useForm } from "react-hook-form";
 import { useState, useEffect } from "react";
@@ -17,16 +18,20 @@ import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 
-export default function LoginPage({ navigation }) {
+// Create the functional component for RegisterPage
+export default function RegisterPage({ navigation }) {
+  // State variables for storing input values
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
+  // Function to handle user registration
   const onRegisterClick = () => {
+    // Make a POST request to the server to perform registration
     axios
       .post(
-        "http://172.21.229.212/index.php/user/register",
+        "http://172.21.9.38/index.php/user/register",
         {
           username: email,
           password: password,
@@ -36,8 +41,7 @@ export default function LoginPage({ navigation }) {
       )
       .then((response) => {
         if (response.data.success) {
-          // Successful register, set the user and store login data
-          console.log(response.data);
+          // Successful registration, set the user and navigate to the "Main" screen
           setUsername(response.data.username);
           navigation.reset({
             index: 0,
@@ -46,29 +50,34 @@ export default function LoginPage({ navigation }) {
             ],
           });
         } else {
+          // If registration fails, show an error message
           Alert.alert("Registration Failed", response.data.message, [
             { text: "OK", onPress: () => console.log("OK Pressed") },
           ]);
         }
       })
       .catch((error) => {
+        // If an error occurs during the request, show an error message
         Alert.alert("Registration Failed", error, [
           { text: "OK", onPress: () => console.log("OK Pressed") },
         ]);
-        // Handle login error, display an error message, etc.
+        // Handle registration error, display an error message, etc.
       });
   };
 
+  // Render the component
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Register Page</Text>
       <KeyboardAwareScrollView>
+        {/* Input for entering the username */}
         <TextInput
           placeholder="Username"
           onChangeText={setEmail}
           value={email}
           style={styles.input}
         />
+        {/* Input for entering the password */}
         <TextInput
           placeholder="Password"
           onChangeText={setPassword}
@@ -76,6 +85,7 @@ export default function LoginPage({ navigation }) {
           secureTextEntry={true}
           style={styles.input}
         />
+        {/* Input for confirming the password */}
         <TextInput
           placeholder="Confirm Password"
           onChangeText={setConfirmPassword}
@@ -83,6 +93,7 @@ export default function LoginPage({ navigation }) {
           secureTextEntry={true}
           style={styles.input}
         />
+        {/* Button for submitting the registration */}
         <View style={styles.button}></View>
         <Button
           title="Register"
@@ -94,6 +105,7 @@ export default function LoginPage({ navigation }) {
   );
 }
 
+// Styles for the component
 const styles = StyleSheet.create({
   container: {
     flex: 1,
